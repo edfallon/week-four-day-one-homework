ec89317ea82b3d191d53bfc6d95b2b85d3ee7aae
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-16 22:11:11 +0100

    added text align

--- models/game.rb
+++ models/game.rb
@@ -1,29 +1,12 @@
 class Game
 
-# def self.rock_wins(hand1, hand2)
-#   if hand1 == "Rock" && hand2 == "Scissors"
-#     return "Rock wins!"
-#   end
-# end
-#
-# def self.scissors_wins(hand1, hand2)
-#   if hand1 == "Scissors" && hand2 == "Paper"
-#     return "Scissors wins!"
-#   end
-# end
-#
-# def self.paper_wins(hand1, hand2)
-#   if hand1 == "Paper" && hand2 == "Rock"
-#     return "Paper wins!"
-#   end
-# end
 def self.play(hand1, hand2)
   if hand1 == "Rock" && hand2 == "Scissors"
-      return "Rock wins!"
+      return "Player 1 wins!"
   elsif hand1 == "Scissors" && hand2 == "Paper"
-      return "Scissors wins!"
+      return "Player 1 wins!"
   elsif hand1 == "Paper" && hand2 == "Rock"
-      return "Paper wins!"
+      return "Player 1 wins!"
   elsif hand1 == "Paper" && hand2 == "Paper"
       return "Draw!"
   elsif hand1 == "Rock" && hand2 == "Rock"
@@ -31,12 +14,12 @@ def self.play(hand1, hand2)
   elsif hand1 == "Scissors" && hand2 == "Scissors"
       return "Draw!"
   elsif hand1 == "Scissors" && hand2 == "Rock"
-      return "Rock wins!"
+      return "Player 2 wins!"
   elsif hand1 == "Paper" && hand2 == "Scissors"
-      return "Scissors wins!"
+      return "Player 2 wins!"
   elsif hand1 == "Rock" && hand2 == "Paper"
-      return "Paper wins!"
-      
+      return "Player 2 wins!"
+
   end
 end
 
