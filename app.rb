a231161ebe88d1764959cdc7c06d8fb5a0e07a7c
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-16 16:16:27 +0100

    files created and rock_wins Added

---
+++ app.rb
@@ -0,0 +1,5 @@
+require("sinatra")
+require("sinatra/contrib/all") if development?
+also_reload("./models/*")
+
+require_relative("./models/game.rb")
---
+++ models/game.rb
@@ -0,0 +1,9 @@
+class Game
+
+def self.rock_wins(hand1, hand2)
+  if hand1 == "Rock" && hand2 == "Scissors"
+    return "Rock wins!"
+  end
+end 
+
+end
