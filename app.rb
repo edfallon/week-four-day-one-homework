b2af1336dd1539bed80c0fddae73031ab58f87fa
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-16 16:44:28 +0100

    self.play added

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ require("sinatra/contrib/all") if development?
 also_reload("./models/*")
 
 require_relative("./models/game.rb")
+
+
+get "/:hand1/:hand2" do
+  result = Game.play(params[:hand1].to_s, params[:hand2].to_s)
+  return result
+end
--- models/game.rb
+++ models/game.rb
@@ -1,9 +1,43 @@
 class Game
 
-def self.rock_wins(hand1, hand2)
+# def self.rock_wins(hand1, hand2)
+#   if hand1 == "Rock" && hand2 == "Scissors"
+#     return "Rock wins!"
+#   end
+# end
+#
+# def self.scissors_wins(hand1, hand2)
+#   if hand1 == "Scissors" && hand2 == "Paper"
+#     return "Scissors wins!"
+#   end
+# end
+#
+# def self.paper_wins(hand1, hand2)
+#   if hand1 == "Paper" && hand2 == "Rock"
+#     return "Paper wins!"
+#   end
+# end
+def self.play(hand1, hand2)
   if hand1 == "Rock" && hand2 == "Scissors"
-    return "Rock wins!"
+      return "Rock wins!"
+  elsif hand1 == "Scissors" && hand2 == "Paper"
+      return "Scissors wins!"
+  elsif hand1 == "Paper" && hand2 == "Rock"
+      return "Paper wins!"
+  elsif hand1 == "Paper" && hand2 == "Paper"
+      return "Draw!"
+  elsif hand1 == "Rock" && hand2 == "Rock"
+      return "Draw!"
+  elsif hand1 == "Scissors" && hand2 == "Scissors"
+      return "Draw!"
+  elsif hand1 == "Scissors" && hand2 == "Rock"
+      return "Rock wins!"
+  elsif hand1 == "Paper" && hand2 == "Scissors"
+      return "Scissors wins!"
+  elsif hand1 == "Rock" && hand2 == "Paper"
+      return "Paper wins!"
+      
   end
-end 
-
 end
+
+end# end of class
